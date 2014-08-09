5ac6182f7974f047ecf71a64dc392cc5a7037718
DreamHeight <guang_li163@163.com>
2014-08-09 21:59:59 +0800

    first

---
+++ PowersDemo/App_Code/AuthorizePowers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PowersDemo.Controllers
+{
+    public class AuthorizePowers : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if(httpContext.Request.Cookies["yourset"]==null&&httpContext.Session["CurrentUser"]==null){
+                return false;
+            }
+            return base.AuthorizeCore(httpContext);
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+            if(filterContext.HttpContext.Response.StatusCode == 403){
+                filterContext.Result = new RedirectResult("/Account/Login");
+            }
+        }
+    }
+}
